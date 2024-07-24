07491acf7d3c5d2355e9775c02a7e9beb4208af8
kjulieth18 <45132485+kjulieth18@users.noreply.github.com>
2024-07-24 14:14:37 -0500

    escenarios

--- Assets/Scripts/SideController.cs
+++ Assets/Scripts/SideController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,10 +13,16 @@ public class SideController : MonoBehaviour
     public InputActionReference playerInput;
     public SpriteRenderer spriteReder;
 
+
+
     // Character variables
 
     public float playerSpeed;
+    public float fuerzaSalto;
+
+    public bool mirandoDerecha = true;
     private Vector2 inputValue;
+
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
@@ -25,15 +32,43 @@ public class SideController : MonoBehaviour
     void Update()
     {
         inputValue = playerInput.action.ReadValue<Vector2>();
+
+        AjustarRotacion(inputValue.x);
     }
 
     public void OnMovement(InputAction.CallbackContext context)
     {
         inputValue = context.ReadValue<Vector2>();
+
+
     }
 
     private void FixedUpdate()
     {
         rBody.velocity = inputValue * playerSpeed;
+    }
+
+    private void AjustarRotacion(float direccionX)
+    {
+        if (direccionX > 0 && !mirandoDerecha)
+        {
+            Girar();
+        }
+        else if (direccionX < 0 && mirandoDerecha)
+        {
+            Girar();
+        }
+    }
+    private void Girar()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        Vector3 escala = transform.localScale;
+        escala.x *= 1;
+        transform.localScale = escala;
+    }
+
+    private void Saltar()
+    {
+        rBody.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);
     }
 }
